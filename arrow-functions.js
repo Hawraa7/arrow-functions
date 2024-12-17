b8a9f29f40102d2ad9a60161141d1faef0f38765
Hawraa Hijazi <hawraahijazi1996@gmail.com>
2024-12-17 18:21:43 +0000

    we will comment the vanilla javascript function to run the arrow function and delete (or) in the code and one of the addtwonumbers2 function

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,35 @@
  */
 
 // Vanilla JavaScript Function
+function addTwonumbers(a,b) {
+    return a +b;
+}
+let sum = addTwonumbers (3,5);
+console.log(sum);
 
 // Arrow Function With Parameters
-
+const addTwonumbers = (a,b) => {
+    return a +b;
+}
+let sum = addTwonumbers (3,5);
+console.log(sum);
 // Single Line Arrow Function With Parameters
+const addTwonumbers2 = (a,b) => (a + b); 
+or
+const addTwonumbers2 = (a,b) => a + b;
+let sum2 = addTwonumbers(6,4);
+console.log(sum2);
 
 // Implicit Returns
+const saysomething = message => console.log(message);
+saysomething('hello there');
+const sayhello = () => console.log('hello');
+sayhello();
 
 // Returning Multiple Lines
+const returnmultiplelines = () => (
+    `<p>
+        this is a string
+    </p>`
+)
+console.log(returnmultiplelines());
